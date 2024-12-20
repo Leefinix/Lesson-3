4dd0cff612ce4b9c7261dfc728d57c0ab919e745
Leefinix <joaquinnleo@gmail.com>
2024-12-20 17:09:27 +0700

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,26 @@
+name = "Leo"
+age = 11
+student = True
+weight = 159
+
+print("Name :", name)
+print("Data type of name: ", type(name))
+
+print("Age :", age)
+print("Data type of age: ", type(age))
+
+print("Student :", student)
+print("Data type of student: ", type(student))
+
+print("Weight :", weight)
+print("Data type of weight: ", type(weight))
+
+print("\n After Typecasting: ")
+
+age = str(age)
+print(age)
+print("Data type of age: ", type(age))
+
+weight = float(weight)
+print(weight)
+print("Data type of weight: ", type(weight))
